38ac18aa435e26c57aa2e9ee0192324ebc716a26
DenysTutov <93935076+DenysTutov@users.noreply.github.com>
2022-01-24 00:04:39 +0200

    добавляет фиксацию кнопки закрытия моб меню

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,13 +1,15 @@
 (() => {
-    const menuBtnRef = document.querySelector("[data-menu-button]");
-    const mobileMenuRef = document.querySelector("[data-menu]");
+    const refs = {
+        openMenuBtn: document.querySelector('.mobile-menu__button-open'),
+        closeMenuBtn: document.querySelector('.mobile-menu__cross-button'),
+        menu: document.querySelector('.mobile-menu__menu-container'),
+    };
 
-    menuBtnRef.addEventListener("click", () => {
-        const expanded = menuBtnRef.getAttribute("aria-expended") === "true" || "false";
+refs.openMenuBtn.addEventListener('click', toggleMenu);
+refs.closeMenuBtn.addEventListener('click', toggleMenu);
 
-        menuBtnRef.classList.toggle("is-open");
-        menuBtnRef.setAttribute("aria-expanded", !expanded);
-        
-        mobileMenuRef.classList.toggle('is-open');
-    });
-})();
+function toggleMenu() {
+    refs.menu.classList.toggle('is-hidden');
+}
+}) ();
+    
